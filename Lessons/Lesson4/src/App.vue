c4949d5acfe1b42d973fbab216c7b424657881f2
Глеб Иванов Евгеньевич <@lcoast.itstep>
2023-01-24 19:36:18 +0600

    24-01-2023-1

---
+++ Lessons/Lesson4/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Button from "./components/Button.vue";
+export default {
+    components: {
+        Button,
+    },
+};
+</script>
+
+<template>
+    <main>
+        <Button text="Gleb"></Button>
+        <Button text="Glob"></Button>
+        <Button text="Glib"></Button>
+    </main>
+</template>
+
+<style scoped></style>
---
+++ Lessons/Lesson4/src/components/Button.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    props: ["text", "onclick"],
+};
+</script>
+
+<template>
+    <button @click="onclick">{{ text }}</button>
+</template>
+
+<style scoped>
+button {
+    padding: 5px;
+}
+</style>
